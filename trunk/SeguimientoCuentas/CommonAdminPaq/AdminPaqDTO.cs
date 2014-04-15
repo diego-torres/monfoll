ec5f2f318fcb1bec7eb7b82f91f31de3e4dac820
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@75c91542-90cd-5bb0-80fd-0a87b8ffc0fb>
2014-04-15 14:55:04 +0000

    Initial definition of components, database script initiated for pgSql.

---
+++ trunk/SeguimientoCuentas/CommonAdminPaq/AdminPaqDTO.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonAdminPaq.dto
+{
+    public class TableNames {
+        public const string EMPRESAS = "MGW00001";
+    }
+    public class IndexNames {
+        public const string EMPRESAS_PK = "PRIMARYKEY";
+    }
+    public class Empresa
+    {
+        // FIELD INDEX IN TABLE
+        public const int ID_EMPRESA = 1;
+        public const int NOMBRE_EMPRESA = 2;
+        public const int RUTA_EMPRESA = 3;
+
+        // FIELD PROPERTIES FOR OBJECT
+        private long id;
+        private string nombre, ruta, alias, resultadoSemanal, tendencia;
+        private IList<Agente> agentes;
+        private IList<long> cSale = new List<long>();
+        private IList<long> cReturn = new List<long>();
+        private IList<string> directores = new List<string>();
+
+        public long Id { get { return id; } set { id = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Ruta { get { return ruta; } set { ruta = value; } }
+        public string Alias { get { return alias; } set { alias = value; } }
+        public IList<Agente> Agentes { get { return agentes; } set { agentes = value; } }
+        public IList<long> CodigosVenta { get { return cSale; } set { cSale = value; } }
+        public IList<long> CodigosDevolucion { get { return cReturn; } set { cReturn = value; } }
+        public IList<string> TelefonosDirectores { get { return directores; } set { directores = value; } }
+        public string ResultadoSemanal { get { return resultadoSemanal; } set { resultadoSemanal = value; } }
+        public string Tendencia { get { return tendencia; } set { tendencia = value; } }
+    }
+    public class Agente
+    {
+
+        public const int ID_AGENTE = 1;
+        public const int CODIGO_AGENTE = 2;
+        public const int NOMBRE_AGENTE = 3;
+
+        private long id;
+        private string codigo;
+        private string nombre;
+        private CellPhone phone;
+
+        public long Id { get { return id; } set { id = value; } }
+        public string Codigo { get { return codigo; } set { codigo = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public CellPhone Phone { get { return phone; } set { phone = value; } }
+    }
+    public class CellPhone {
+        private string phoneNumber;
+        private double metaSemanal;
+        private double vendido4S;
+        private double vendidoSemana;
+        
+        public string PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+        public double MetaSemanal { get { return metaSemanal; } set { metaSemanal = value; } }
+        public double Vendido4S { get { return vendido4S; } set { vendido4S = value; } }
+        public double VendidoSemana { get { return vendidoSemana; } set { vendidoSemana = value; } }
+        
+        public double CumplimientoTendencia 
+        {
+            get 
+            {
+                if (metaSemanal == 0) return 0;
+                return (vendido4S) / (metaSemanal * 4);
+            }
+        }
+
+        public double CumplimientoSemana
+        {
+            get 
+            {
+                if (metaSemanal == 0) return 0;
+                return vendidoSemana / metaSemanal;
+            }
+        }
+
+        public double FaltanteTendencia
+        {
+            get
+            {
+                if (this.CumplimientoTendencia >= 1) return 0;
+                return (metaSemanal * 4) - vendido4S;
+            }
+        }
+
+        public double FaltanteSemana
+        {
+            get
+            {
+                if(this.CumplimientoSemana>=1) return 0;
+                return metaSemanal - vendidoSemana;
+            }
+        }
+
+    }
+}
---
+++ trunk/SeguimientoCuentas/CommonAdminPaq/AdminPaqImpl.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonAdminPaq.dto;
+
+namespace CommonAdminPaq
+{
+    public class AdminPaqImpl
+    {
+        private IList<Empresa> empresas = new List<Empresa>();
+        private AdminPaqLib lib;
+
+        public IList<Empresa> Empresas { get { return empresas; } set { empresas = value; } }
+        
+        public AdminPaqImpl() {
+            lib = new AdminPaqLib();
+            lib.SetDllFolder();
+        }
+
+        public void InitializeSDK() {
+            int connEmpresas, dbResponse, fieldResponse;
+            connEmpresas = AdminPaqLib.dbLogIn("", lib.DataDirectory);
+
+            if (connEmpresas == 0)
+            {
+                ErrLogger.Log("No se pudo crear conexión a la tabla de Empresas de AdminPAQ.");
+                return;
+            }
+
+            dbResponse = AdminPaqLib.dbGetTopNoLock(connEmpresas, TableNames.EMPRESAS, IndexNames.EMPRESAS_PK);
+            while (dbResponse == 0)
+            {
+                Empresa empresa = new Empresa();
+                
+                int idEmpresa = 0;
+                fieldResponse = AdminPaqLib.dbFieldLong(connEmpresas, TableNames.EMPRESAS, Empresa.ID_EMPRESA, ref idEmpresa);
+                empresa.Id = idEmpresa;
+
+                StringBuilder nombreEmpresa = new StringBuilder(151);
+                fieldResponse = AdminPaqLib.dbFieldChar(connEmpresas, TableNames.EMPRESAS, Empresa.NOMBRE_EMPRESA, nombreEmpresa, 151);
+                string sNombreEmpresa = nombreEmpresa.ToString(0, 150).Trim();
+                empresa.Nombre = sNombreEmpresa;
+
+                StringBuilder rutaEmpresa = new StringBuilder(254);
+                fieldResponse = AdminPaqLib.dbFieldChar(connEmpresas, TableNames.EMPRESAS, Empresa.RUTA_EMPRESA, rutaEmpresa, 254);
+                string sRutaEmpresa = rutaEmpresa.ToString(0, 253).Trim();
+                empresa.Ruta = sRutaEmpresa;
+
+                empresas.Add(empresa);
+                dbResponse = AdminPaqLib.dbSkip(connEmpresas, TableNames.EMPRESAS, IndexNames.EMPRESAS_PK, 1);
+            }
+
+            AdminPaqLib.dbLogOut(connEmpresas);
+        }
+
+        public void RetrieveSales(Empresa empresa)
+        {
+            DateTime today = DateTime.Today;
+            int weekStartDelta = 2 - (int)today.DayOfWeek;
+            DateTime weekStart = today.AddDays(weekStartDelta);
+            DateTime weeksAgo = weekStart.AddDays(-21);
+
+            DateTime saleDate = weeksAgo;
+
+            //KeyValuePair<string, long> connections = new KeyValuePair<string,long>();
+            Dictionary<string, int> connections = new Dictionary<string, int>();
+            connections.Add("documents", AdminPaqLib.dbLogIn("", empresa.Ruta));
+            connections.Add("concepts", AdminPaqLib.dbLogIn("", empresa.Ruta));
+            
+            while (saleDate <= today) 
+            {
+                RetrieveSales(saleDate, empresa, connections);
+                saleDate = saleDate.AddDays(1);
+            }
+
+            ReleaseConnections(connections);
+
+        }
+
+        private void ReleaseConnections(Dictionary<string, int> connections)
+        {
+            foreach (KeyValuePair<string, int> entry in connections)
+            {
+                AdminPaqLib.dbLogOut(entry.Value);
+            }
+        }
+
+        private bool dateInCurrentWeek(DateTime date)
+        {
+            DateTime today = DateTime.Today;
+            int weekStartDelta = 2 - (int)today.DayOfWeek;
+            DateTime weekStart = today.AddDays(weekStartDelta);
+            return date.CompareTo(weekStart) >= 0;
+        }
+
+        private long conceptCode(int conceptId, Dictionary<string, int> connections)
+        {
+            int connection, dbResponse, spaceCounter;
+            bool connected = connections.TryGetValue("concepts", out connection);
+            StringBuilder sbConceptCode = new StringBuilder(30);
+
+            if (!connected || connection == 0)
+            {
+                ErrLogger.Log("Connection not allowed in adminpaq for concepts");
+                return 0;
+            }
+
+            spaceCounter = 11 - conceptId.ToString().Length;
+            string key = new string(' ', spaceCounter);
+            key = key + conceptId.ToString();
+
+            dbResponse = AdminPaqLib.dbGetNoLock(connection, "MGW10006", "PRIMARYKEY", key);
+            if (dbResponse == 0)
+            {
+                int fqResult = AdminPaqLib.dbFieldChar(connection, "MGW10006", 2, sbConceptCode, 30);
+                string sConcept = sbConceptCode.ToString().Substring(0, 29).Trim();
+                long result = 0;
+                bool casted = long.TryParse(sConcept, out result);
+                if (!casted) return 0;
+
+                return result;
+            }
+
+            return 0;
+            
+        }
+
+        private bool agentInCompany(int agentId, Empresa empresa)
+        {
+            foreach (Agente agent in empresa.Agentes)
+            {
+                if (agent.Id == agentId)
+                    return true;
+            }
+            return false;
+        }
+
+        private void addSale(int agentId, Empresa empresa, double sold, DateTime saleDate) 
+        { 
+            foreach(Agente agent in empresa.Agentes)
+            {
+                if (agent.Id != agentId) continue;
+
+                agent.Phone.Vendido4S += sold;
+                if (dateInCurrentWeek(saleDate))
+                    agent.Phone.VendidoSemana += sold;
+            }
+        }
+
+        private void RetrieveSales(DateTime date, Empresa empresa, Dictionary<string, int> connections) 
+        {
+
+            string filterDate;
+            int cancelled, returned, conceptId, agentId, currencyId, connection, dbResponse;
+            double changeValue, sold;
+            StringBuilder docDate = new StringBuilder(9);
+            long conceptCode;
+
+            bool isSale, isReturn, validAgent, connected;
+
+            connected = connections.TryGetValue("documents", out connection);
+            if (!connected || connection == 0) 
+            {
+                ErrLogger.Log("Connection not allowed in adminpaq for company [" + empresa.Nombre + "] @ route [" + empresa.Ruta + "]");
+                return;
+            }
+
+            filterDate = date.ToString("yyyyMMdd");
+            dbResponse = AdminPaqLib.dbGetNoLock(connection, "MGW10008", "CFECHA", filterDate);
+            while (dbResponse == 0)
+            {
+                int fqResult = 0;
+                cancelled = 0;
+                fqResult = AdminPaqLib.dbFieldLong(connection, "MGW10008", 26, ref cancelled);
+                if (cancelled == 1) 
+                {
+                    dbResponse = AdminPaqLib.dbSkip(connection, "MGW10008", "CFECHA", 1);
+                    continue;
+                }
+
+                returned = 0;
+                fqResult = AdminPaqLib.dbFieldLong(connection, "MGW10008", 27, ref returned);
+                if (returned == 1)
+                {
+                    dbResponse = AdminPaqLib.dbSkip(connection, "MGW10008", "CFECHA", 1);
+                    continue;
+                }
+
+                fqResult = AdminPaqLib.dbFieldChar(connection, "MGW10008", 6, docDate, 9);
+                string sDocDate = docDate.ToString().Substring(0, 8).Trim();
+                if (!filterDate.Equals(sDocDate)) break;
+
+                conceptId = 0;
+                fqResult = AdminPaqLib.dbFieldLong(connection, "MGW10008", 3, ref conceptId);
+                conceptCode = this.conceptCode(conceptId, connections);
+
+                isReturn = false;
+                isSale = empresa.CodigosVenta.Contains(conceptCode);
+                if(!isSale) isReturn = empresa.CodigosDevolucion.Contains(conceptCode);
+
+                if (!isSale && !isReturn)
+                {
+                    dbResponse = AdminPaqLib.dbSkip(connection, "MGW10008", "CFECHA", 1);
+                    continue;
+                }
+
+                agentId = 0;
+                fqResult = AdminPaqLib.dbFieldLong(connection, "MGW10008", 10, ref agentId);
+                validAgent = agentInCompany(agentId, empresa);
+
+                if (!validAgent)
+                {
+                    dbResponse = AdminPaqLib.dbSkip(connection, "MGW10008", "CFECHA", 1);
+                    continue;
+                }
+
+
+                // Currency 1 = Pesos Mexicanos
+                currencyId = 1;
+                changeValue = 1;
+                fqResult = AdminPaqLib.dbFieldLong(connection, "MGW10008", 15, ref currencyId);
+
+                if (currencyId != 1) 
+                {
+                    fqResult = AdminPaqLib.dbFieldDouble(connection, "MGW10008", 16, ref changeValue);
+                }
+
+                sold = 0;
+                fqResult = AdminPaqLib.dbFieldDouble(connection, "MGW10008", 31, ref sold);
+
+                if (isReturn) sold *= -1;
+                if (currencyId != 1) sold *= changeValue;
+
+                addSale(agentId, empresa, sold, date);
+
+                dbResponse = AdminPaqLib.dbSkip(connection, "MGW10008", "CFECHA", 1);
+            }
+        }
+
+    }
+}
