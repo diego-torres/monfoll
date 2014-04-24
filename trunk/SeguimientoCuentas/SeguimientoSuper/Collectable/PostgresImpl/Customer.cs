d6b978cc559e18808a4941cb54e1aeee8bdb397e
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@75c91542-90cd-5bb0-80fd-0a87b8ffc0fb>
2014-04-24 04:49:26 +0000

    Retrieves collectable accounts from adminpaq and organizes customers list.

---
+++ trunk/SeguimientoCuentas/SeguimientoSuper/Collectable/PostgresImpl/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Npgsql;
+
+namespace SeguimientoSuper.Collectable.PostgresImpl
+{
+    public class Customer : CommonBase
+    {
+        public DataTable ReadCustomers()
+        {
+            DataSet ds = new DataSet();
+            NpgsqlDataAdapter da;
+            string sqlString = "SELECT ID_CLIENTE, CD_CLIENTE, NOMBRE_CLIENTE, RUTA, DIA_PAGO " +
+                "FROM CAT_CLIENTE;";
+
+            if (conn == null || conn.State != ConnectionState.Open)
+                connect();
+
+            da = new NpgsqlDataAdapter(sqlString, conn);
+
+            ds.Reset();
+            da.Fill(ds);
+            conn.Close();
+            return ds.Tables[0];
+        }
+    }
+}
--- trunk/SeguimientoCuentas/SeguimientoSuper/Config/FormDownload.Designer.cs
+++ trunk/SeguimientoCuentas/SeguimientoSuper/Config/FormDownload.Designer.cs
@@ -28,11 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descargando datos de AdminPaq ...";
+            // 
+            // FormDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(365, 120);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormDownload";
             this.Text = "FormDownload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
