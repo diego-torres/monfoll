d69bccfb002e073eafe182ae2d696d55a6ade1e9
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@75c91542-90cd-5bb0-80fd-0a87b8ffc0fb>
2014-06-25 15:51:38 +0000

    Setting version details.

--- SeguimientoCuentas/SeguimientoCobrador/FormMain.Designer.cs
+++ SeguimientoCuentas/SeguimientoCobrador/FormMain.Designer.cs
@@ -123,7 +123,7 @@
             // 
             this.mainStatus.Name = "mainStatus";
             this.mainStatus.Size = new System.Drawing.Size(311, 17);
-            this.mainStatus.Text = "Seguimiento a cuentas por cobrar - módulo de cobrador V 3.1.0";
+            this.mainStatus.Text = "Seguimiento a cuentas por cobrar - módulo de cobrador V 3.1.1";
             // 
             // mainProgressBar
             // 
