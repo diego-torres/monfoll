acf648f8e80448a65a5e191aadb156d66296113a
j.alfredo.pacheco@gmail.com <j.alfredo.pacheco@gmail.com@75c91542-90cd-5bb0-80fd-0a87b8ffc0fb>
2014-11-02 01:01:36 +0000

    GrabarPorVencer prepared for testing.

--- trunk/SeguimientoCuentas/ConsolaODBCFox/ado/PgGruposVencimiento.cs
+++ trunk/SeguimientoCuentas/ConsolaODBCFox/ado/PgGruposVencimiento.cs
@@ -110,6 +110,106 @@ namespace ConsolaODBCFox.ado
             }
         }
 
+        public static void GrabarPorVencer(List<FactPorVencer> porvencer, GrupoVencimiento grupo)
+        {
+            List<FactPorVencer> listAllFactPorVencerInDB = new List<FactPorVencer>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings[Config.Common.JASPER].ConnectionString;
+
+            using (NpgsqlConnection pgConnection = new NpgsqlConnection(connectionString))
+            {
+                pgConnection.Open();
+                NpgsqlDataReader dr;
+                NpgsqlCommand cmd;
+
+                string sqlString = "SELECT id_cliente, id_grupo_vencimiento, saldo_por_vencer " +
+                                    "FROM fact_por_vencer;";
+
+                cmd = new NpgsqlCommand(sqlString, pgConnection);
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    FactPorVencer oFactPorVencer = new FactPorVencer();
+                    oFactPorVencer.IdCliente = int.Parse(dr["id_cliente"].ToString());
+                    //oFactVencimiento.IdEmpresa = int.Parse(dr[""].ToString()); //TODO
+                    oFactPorVencer.IdVencimiento = int.Parse(dr["id_grupo_vencimiento"].ToString());
+                    oFactPorVencer.Importe = int.Parse(dr["saldo_por_vencer"].ToString());
+                    listAllFactPorVencerInDB.Add(oFactPorVencer);
+                }
+
+                dr.Close();
+                cmd.Dispose();
+                pgConnection.Close();
+            }
+
+
+            // Para cada ID Doco en la BD de postgres
+            using (NpgsqlConnection pgConnection = new NpgsqlConnection(connectionString))
+            {
+                try
+                {
+                    pgConnection.Open();
+
+                    using (NpgsqlTransaction transaction = pgConnection.BeginTransaction())
+                    {
+
+                        try
+                        {
+                            foreach (FactPorVencer currentPorVencer in porvencer)
+                            {
+                                NpgsqlCommand theCommand;
+                                FactPorVencer oFactPorVencerToPersist = currentPorVencer;
+
+                                // Check if record exists.
+                                FactPorVencer oFactPorVencerExistente = listAllFactPorVencerInDB.First(record => record.IdCliente == currentPorVencer.IdCliente && record.IdEmpresa == currentPorVencer.IdEmpresa && record.IdVencimiento == currentPorVencer.IdVencimiento);
+
+                                // if record exists, update the record.
+                                if (oFactPorVencerExistente != null)
+                                {
+                                    oFactPorVencerToPersist.Importe += oFactPorVencerExistente.Importe;
+                                    string sqlUpdateVencimiento = "UPDATE fact_por_vencer " +
+                                                                    "SET saldo_por_vencer=@saldo_por_vencer " +
+                                                                    "WHERE id_cliente=@id_cliente and id_grupo_vencimiento=@id_grupo_vencimiento;";
+                                    theCommand = new NpgsqlCommand(sqlUpdateVencimiento, pgConnection);
+                                }
+                                // if record does not exists, insert the record
+                                else
+                                {
+                                    string sqlUpdateVencimiento = "INSERT INTO fact_por_vencer(" +
+                                                                    "id_cliente, id_grupo_vencimiento, saldo_por_vencer) " +
+                                                                    "VALUES (@id_cliente, @id_grupo_vencimiento, @saldo_por_vencer);";
+                                    theCommand = new NpgsqlCommand(sqlUpdateVencimiento, pgConnection);
+                                }
+
+                                theCommand.Parameters.Add("@saldo_por_vencer", NpgsqlTypes.NpgsqlDbType.Double);
+                                theCommand.Parameters.Add("@id_cliente", NpgsqlTypes.NpgsqlDbType.Integer);
+                                theCommand.Parameters.Add("@id_grupo_vencimiento", NpgsqlTypes.NpgsqlDbType.Integer);
+
+                                theCommand.Parameters["@saldo_por_vencer"].Value = oFactPorVencerToPersist.Importe;
+                                theCommand.Parameters["@id_cliente"].Value = oFactPorVencerToPersist.IdCliente;
+                                theCommand.Parameters["@id_grupo_vencimiento"].Value = grupo.Id;
+
+                                theCommand.ExecuteNonQuery();
+
+                            }
+                            transaction.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            if (transaction != null) transaction.Rollback();
+                            throw ex;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                pgConnection.Close();
+            }
+        }
+
         private static int IdCliente(NpgsqlConnection conn, string codigoCliente, int idEmpresa)
         {
             int result = 0;
